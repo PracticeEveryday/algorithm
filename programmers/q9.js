5c7c1064a7922441d483c051d5c562b949f1ab33
PracticeEveryday <developerforus@gmail.com>
2024-01-01 15:09:12 +0900

    feat: try2 with 유클리드 호제법

--- programmers/q9.js
+++ programmers/q9.js
@@ -2,45 +2,18 @@
 // 최소 공배수란 배수 중 공통이 되는 가장 작은 숫자이다.
 
 function solution(arr) {
-    // 공통으로 포함되는 약수와 각각의 서로소로 곱하기
-    const divided = []
-    for(const val of arr) {
-        const divisosArr = findDivisors(val);
-        // 공통만 빼내기
-        divided.push(divisosArr);
-    }
-    const commonArr = findCommonElements(...divided)
-    const larged = commonArr[commonArr.length - 1]
-
-    const seoloso = arr.map((item) => item / larged)
-    const set = new Set([...commonArr, ...seoloso])
-
-    return [...set].reduce((a, b) => a * b)
-
-}
-
-function findCommonElements(...arrays) {
-    if (arrays.length === 0) {
-        return [];
-    }
-    const baseArray = arrays[0];
-    return baseArray.filter(element => arrays.every(arr => arr.includes(element)));
-
-}
-
-
-function findDivisors(number) {
-    let divisors = [];
-
-    for (let i = 1; i <= number; i++) {
-        if (number % i === 0) {
-            divisors.push(i);
-        }
+    let lastLcm = uclid(arr[0], arr[1]);
+    for(let i = 1 ; i < arr.length; i++) {
+        lastLcm = uclid(lastLcm, arr[i])
     }
 
-    return divisors;
+    return lastLcm
 }
-
+function uclid(num1, num2) {
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    const lcm = (a, b) => a * b / gcd(a, b);
+    return  lcm(num1, num2);
+}
 
 
 console.log(solution([2, 6, 8, 14])) // 168
