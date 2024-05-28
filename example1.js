a59e4323257e7dd21851a0be4cde3b301bb961a7
PracticeEveryday <developerforus@gmail.com>
2024-05-28 21:02:08 +0900

    feat: 구조분해 할당 공부

--- example1.js
+++ example1.js
@@ -1,66 +1,32 @@
-function addUpTo(n) {
-    let total = 0;
-    // for loop
-    for (let i = 1; i <= n; i++) {
-        total += i;
-    }
-    return total
-}
+// function func () {}
+//
+// console.log(func.name); // func
+//
+// func.name = "hello";
+// console.log(func.name); // func
+//
+//
+// Reflect.defineProperty(func, "name", { writable: true })
+// func.name = "what"
+// console.log(func.name) // 'what'
+//
+// console.log(Reflect.getOwnPropertyDescriptor(func, "name"))
 
-console.time("a")
-console.log(addUpTo(100000000));
-console.timeEnd("a")
 
-function addUpToV2(n) {
-    // 수학 공식...
-    return n * (n + 1) / 2;
-}
-console.time("b")
-console.log(addUpToV2(100000000));
-console.timeEnd("b")
 
-function countUpAndDown(n) {
-    // for loop
-    for (let i = 1; i <= n; i++) {
-        console.log(i)
-    }
+let title, hello;
+const obj = {titl: "title2", hello: "hello2"};
 
-    for (let i = n; i >= 0; i--) {
-        console.log(i)
-    }
-}
+({titl, hello} = obj);
+const test = ({titl, hello} = obj);
+console.log(titl, hello)
 
-console.time("c")
-countUpAndDown(100);
-console.timeEnd("c")
+// console.log(test, "test")
+// console.log(({titl, hello} = obj))
 
-function printAllPairs(n) {
-    for(let i =0; i< n; i ++){
-        for (let j = 0; j<n; j++) {
-            consoe.log(i, j)
-        }
-    }
-}
+console.log(title, "title")
+console.log(titl, "titl")
 
-function logAtLeast5(n) {
-    for (let i = 1; i<= Math.min(5, n); i++){
-        console.log(i)
-    }
-}
 
-function sum(arr) {
-    let total = 0;
-    for(let i =0; i< arr.length; i++){
-        total += arr [i];
-    }
-    return total
-}
-
-function double(arr) {
-    let newArr = [];
-    for (let i = 0 ; i< arr.length ; i++){
-        newArr.push(2 * arr[i]);
-    }
-
-    return newArr
-}
+const wow = titl;
+console.log(wow, "wow")
