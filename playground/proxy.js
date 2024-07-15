eb087551486a823aed0ead1893d3fa4b79188ae6
PracticeEveryday <developerforus@gmail.com>
2024-07-15 23:49:09 +0900

    feat: 속성 접근자 예시 코드

--- playground/proxy.js
+++ playground/proxy.js
@@ -12,24 +12,71 @@
 // console.log("Proxy a:", proxy.a); // "a" 출력 및 로그
 // console.log("Target a:", target.a); // "a" 출력 및 로그
 
+//
+// let book = {
+//     _title: "JavaScript",
+//
+//     // getter 함수
+//     get title() {
+//         return this._title;
+//     },
+//
+//     // setter 함수
+//     set title(newTitle) {
+//         if (typeof newTitle === 'string' && newTitle.length > 0) {
+//             this._title = newTitle;
+//         } else {
+//             console.log("Invalid title");
+//         }
+//     }
+// };
+//
+// console.log(book.title); // JavaScript
+// book.title = "";    // Invalid title
 
-let book = {
-    _title: "JavaScript",
 
-    // getter 함수
-    get title() {
-        return this._title;
-    },
+let person = {};
 
-    // setter 함수
-    set title(newTitle) {
-        if (typeof newTitle === 'string' && newTitle.length > 0) {
-            this._title = newTitle;
-        } else {
-            console.log("Invalid title");
-        }
-    }
-};
+Object.defineProperty(person, 'name', {
+    value: '김동현',
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
 
-console.log(book.title); // JavaScript
-book.title = "";    // Invalid title
+console.log(person.name); // "김동현"
+
+person.name = '김동현22'; // 값 변경
+console.log(person.name); // "Bob"
+
+for (const personKey in person) {
+    console.log(personKey); // 'name'
+}
+
+Object.defineProperty(person, 'age', {
+    value: 11,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+
+for (const personKey in person) {
+    console.log(personKey); // 'name'
+}
+
+Object.defineProperty(person, 'address', {
+    value: "망원동",
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+console.log(Object.keys(person)); // ['name', 'address']
+
+Object.defineProperty(person, 'hidden', {
+    configurable: false // 변경 불가능하게 설정
+});
+
+// 다음 코드는 오류를 발생시킵니다.
+// TypeError: Cannot redefine property: hidden
+Object.defineProperty(person, 'hidden', { enumerable: true });
