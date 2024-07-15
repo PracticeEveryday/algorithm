ab2ef292ca2b7ba70b3e83ec949ab9fb710958fa
PracticeEveryday <developerforus@gmail.com>
2024-07-15 23:45:21 +0900

    feat: 속성 접근자 예시

--- playground/proxy.js
+++ playground/proxy.js
@@ -1,13 +1,35 @@
-const target = { a: "a", b: "b" };
+// const target = { a: "a", b: "b" };
+//
+// const proxy = new Proxy(target, {
+//     get: function(target, property, receiver) {
+//         console.log("Target object:", target);
+//         console.log("Property accessed:", property);
+//         console.log("Receiver:", receiver);
+//         return target[property];
+//     }
+// });
+//
+// console.log("Proxy a:", proxy.a); // "a" 출력 및 로그
+// console.log("Target a:", target.a); // "a" 출력 및 로그
 
-const proxy = new Proxy(target, {
-    get: function(target, property, receiver) {
-        console.log("Target object:", target);
-        console.log("Property accessed:", property);
-        console.log("Receiver:", receiver);
-        return target[property];
+
+let book = {
+    _title: "JavaScript",
+
+    // getter 함수
+    get title() {
+        return this._title;
+    },
+
+    // setter 함수
+    set title(newTitle) {
+        if (typeof newTitle === 'string' && newTitle.length > 0) {
+            this._title = newTitle;
+        } else {
+            console.log("Invalid title");
+        }
     }
-});
+};
 
-console.log("Proxy a:", proxy.a); // "a" 출력 및 로그
-console.log("Target a:", target.a); // "a" 출력 및 로그
+console.log(book.title); // JavaScript
+book.title = "";    // Invalid title
